7a8cf63e06547366cde48ceec628faed2882f90c
baccrie <bakarerilwan1@gmail.com>
2023-09-11 21:54:04 +0100

    Task done

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ from fastapi import FastAPI
 from datetime import datetime
 
 app = FastAPI()
-date = datetime.now()
-utc = datetime.utcnow().isoformat() + "Z"
+day = datetime.now().strftime("%A")
+utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 
 @app.get('/api')
 def api(slack_name: str, track: str):
     return {
         "slack_name": f"{slack_name}",
-        "current_day": f"{date.strftime('%A')}",
-        "utc_time": f"{utc}",
+        "current_day": f"{day}",
+        "utc_time": f"{utc_time}",
         "track": f"{track}",
-        "github_file_url": "https://github.com/baccrie/Hng_Internship_Task1",
-        "github_repo_url": "https://github.com/baccrie/Hng_Internship_Task1/blob/main/api.py",
+        "github_file_url": "https://github.com/baccrie/Hng_Internship_Task1/blob/main/main.py",
+        "github_repo_url": "https://github.com/baccrie/Hng_Internship_Task1",
         "status_code": 200
     }
